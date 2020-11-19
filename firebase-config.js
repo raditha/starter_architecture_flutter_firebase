0912602c2bbb6c4fdc96e55bc22b4df05677b8c6
damithd <ddrwijethunge@gmail.com>
2020-11-19 18:46:01 +0530

    added maps

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export  var firebaseConfig = {
+    apiKey: "AIzaSyC9111kcFeI-HjLFokPFpDZoEjh5OCVycg",
+    authDomain: "medapp-fd8d7.firebaseapp.com",
+    databaseURL: "https://medapp-fd8d7.firebaseio.com",
+    projectId: "medapp-fd8d7",
+    storageBucket: "medapp-fd8d7.appspot.com",
+    messagingSenderId: "488901552868",
+    appId: "1:488901552868:web:0e7a85501ac208e23f1af1",
+    measurementId: "G-Z0T1BQW3WP"
+  };
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 //import 'package:auth_widget_builder/auth_widget_builder.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:starter_architecture_flutter_firebase/app/auth_widget.dart';
-import 'package:starter_architecture_flutter_firebase/app/home/home_page.dart';
+//import 'package:starter_architecture_flutter_firebase/app/auth_widget.dart';
+import 'package:starter_architecture_flutter_firebase/app/map_widget.dart';
+//import 'package:starter_architecture_flutter_firebase/app/home/home_page.dart';
 import 'package:starter_architecture_flutter_firebase/app/top_level_providers.dart';
-import 'package:starter_architecture_flutter_firebase/app/sign_in/sign_in_page.dart';
+//import 'package:starter_architecture_flutter_firebase/app/sign_in/sign_in_page.dart';
 import 'package:starter_architecture_flutter_firebase/routing/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -24,10 +25,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.indigo),
       debugShowCheckedModeBanner: false,
-      home: AuthWidget(
-        nonSignedInBuilder: (_) => SignInPageBuilder(),
-        signedInBuilder: (_) => HomePage(),
-      ),
+       home: MapSample(),
+      // home: AuthWidget(
+      //   nonSignedInBuilder: (_) => SignInPageBuilder(),
+      //   signedInBuilder: (_) => HomePage(),
+      // ),
       onGenerateRoute: (settings) =>
           AppRouter.onGenerateRoute(settings, firebaseAuth),
     );
